6467ace8fbe4d3afe7f8578b6db892427268787b
Jrosero21 <jnrosero@gmail.com>
2025-08-06 20:47:58 -0700

    fixed DB push issues

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,13 +3,20 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  // 1) Where your Drizzle schema lives:
   schema: "./src/server/db/schema.ts",
-
-  // 2) Use the SingleStore dialect:
   dialect: "singlestore",
+  out: "./drizzle/migrations",
+  dbCredentials: {
+    host:     process.env.SINGLESTORE_HOST!,
+    port:     Number(process.env.SINGLESTORE_PORT!),
+    user:     process.env.SINGLESTORE_USER!,
+    password: process.env.SINGLESTORE_PASS!,
+    database: process.env.SINGLESTORE_DB_NAME!,
 
-
-  // 4) (Optional) only apply tables matching your prefix
+    // ← this enables TLS but skips loading any CA bundle
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   tablesFilter: ["drive_tutorial_*"],
 });
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,33 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { singlestoreTable, int, text } from "drizzle-orm/singlestore-core";
+import { singlestoreTable, int, text, index, singlestoreTableCreator } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id:   int("id").primaryKey().autoincrement(),
+
+export const createTable = singlestoreTableCreator(
+  (name) => `drive-tutorial_${name}`,
+);
+
+export const files = createTable("files_table",
+   {
+  id: int("id").primaryKey().autoincrement(),
   name: text("name"),
-  age:  int("age"),
-});
+  size: int("size"),
+  url: text("url"),
+  parent: int("parent").notNull(),
+}, 
+(t) => {
+  return [index("parent_index").on(t.parent)]
+},
+);
 
-
-;
+export const folders = createTable("folders_table",
+  {
+ id: int("id").primaryKey().autoincrement(),
+ name: text("name").notNull(),
+ parent: int("parent"),
+}, 
+(t) => {
+ return [index("parent_index").on(t.parent)]
+},
+);
