f75519ae92d2ec886078ccd12da8aa7d5a28b366
Jrosero21 <jnrosero@gmail.com>
2025-08-08 07:51:54 -0700

    clean up for data access layer

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,41 +1,42 @@
+import "server-only";
+
 import { db } from "~/server/db";
 import {
-    files_table as filesSchema,
-    folders_table as foldersSchema,
+  files_table as filesSchema,
+  folders_table as foldersSchema,
 } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
 
-export async function getAllParentsForFolder(folderId: number) {
-const parents = [];
-let currentId: number | null = folderId;
-while (currentId !== null) {
-    const folder = await db
-    .selectDistinct()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.id, currentId));
+export const QUERIES = {
+  getAllParentsForFolder: async function (folderId: number) {
+    const parents = [];
+    let currentId: number | null = folderId;
+    while (currentId !== null) {
+      const folder = await db
+        .selectDistinct()
+        .from(foldersSchema)
+        .where(eq(foldersSchema.id, currentId));
 
-    if (!folder[0]) {
+      if (!folder[0]) {
         throw new Error("Parent folder not found");
+      }
+      parents.unshift(folder[0]);
+      currentId = folder[0]?.parent;
     }
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent;
-}
-return parents;
-}
+    return parents;
+  },
 
-
-export function getFolders(folderId: number) {
+  getFolders: function (folderId: number) {
     return db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent, folderId));
-}
-    
-    export function getFiles(folderId: number) {
-        return db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.parent, folderId));
+      .select()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.parent, folderId));
+  },
 
-
-}
+  getFiles: function (folderId: number) {
+    return db
+      .select()
+      .from(filesSchema)
+      .where(eq(filesSchema.parent, folderId));
+  },
+};
